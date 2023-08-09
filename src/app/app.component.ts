46513ba973cfb905a2d8f4a2a83486f3a18f126d
DoroshMaggotta <dorosh0508@ukr.net>
2023-08-09 23:14:09 +0300

    AngularSPA1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {GetCardService} from "./service/get-card.service";
 import {CardInterface} from "./interface/cardInterface";
+import {OnInit} from "@angular/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
   title = 'frontend-test';
   public cardArr: Array<CardInterface> = [];
+
   constructor(private getCardService: GetCardService) {
   }
 
   getCard() {
-    this.getCardService.getInfo().subscribe((data: any) => {this.cardArr = data.results})
-}
+    // this.getCardService.getInfo().subscribe((data: any) => {this.cardArr = data.results})
+    this.getCardService.getArticle()
+  }
+
+  async ngOnInit() {
+    this.cardArr = await this.getCardService.getInfo();
+    console.log(this.cardArr)
+  }
 }
--- src/app/service/get-card.service.ts
+++ src/app/service/get-card.service.ts
@@ -1,13 +1,28 @@
-import { Injectable } from '@angular/core';
-import { HttpClient} from "@angular/common/http";
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {CardInterface} from "../interface/cardInterface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetCardService {
+  public cardArr: Array<CardInterface> = [];
+  public articleUrl = "https://spacenews.com/redwire-to-supply-cameras-for-true-anomalys-inspector-satellites/";
   private baseURL = 'https://api.spaceflightnewsapi.net/v4/articles/'
-  constructor(private http: HttpClient) { }
-  getInfo() {
-    return  this.http.get(this.baseURL)
-    }
+
+  constructor(private http: HttpClient) {
+  }
+
+  async getInfo(): Promise<Array<CardInterface>> {
+    const data: any = await this.http.get(this.baseURL).toPromise()
+    return data.results
+  }
+
+  async getArticle(): Promise<any> {
+    const data: any = await this.http.get(this.articleUrl).toPromise()
+    console.log(data)
+    return data;
+  }
+
+
 }
